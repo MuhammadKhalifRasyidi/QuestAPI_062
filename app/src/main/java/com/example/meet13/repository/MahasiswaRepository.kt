a32aa88691828466287790a97bc5c59f776e4846
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2024-12-30 14:02:16 +0700

    Implementasi NetworkMahasiswaRepository untuk menghubungkan operasi CRUD mahasiswa dengan API. Menangani request API untuk CRUD dan mengambil data mahasiswa

--- app/src/main/java/com/example/meet13/repository/MahasiswaRepository.kt
+++ app/src/main/java/com/example/meet13/repository/MahasiswaRepository.kt
@@ -1,6 +1,8 @@
 package com.example.meet13.repository
 
 import com.example.meet13.model.Mahasiswa
+import com.example.meet13.service.MahasiswaService
+import java.io.IOException
 
 interface MahasiswaRepository {
     suspend fun getMahasiswa(): List<Mahasiswa>
@@ -14,3 +16,36 @@ interface MahasiswaRepository {
     suspend fun getMahasiswaByNim(nim: String): Mahasiswa
 }
 
+class NetworkMahasiswaRepository(
+    private val mahasiswaApiService: MahasiswaService
+) : MahasiswaRepository {
+    override suspend fun insertMahasiswa(mahasiswa: Mahasiswa) {
+        mahasiswaApiService.insertMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa) {
+        mahasiswaApiService.updateMahasiswa(nim, mahasiswa)
+    }
+
+    override suspend fun deleteMahasiswa(nim: String) {
+        try {
+            val response = mahasiswaApiService.deleteMahasiswa(nim)
+            if (!response.isSuccessful) {
+                throw IOException("Failed to delete mahasiswa. HTTP Status code: " +
+                    "${response.code()}")
+            } else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e:Exception) {
+            throw e
+        }
+    }
+
+    override suspend fun getMahasiswa(): List<Mahasiswa> =
+        mahasiswaApiService.getMahasiswa()
+
+    override suspend fun getMahasiswaByNim(nim: String): Mahasiswa {
+        return mahasiswaApiService.getMahasiswaByNim(nim)
+    }
+}
