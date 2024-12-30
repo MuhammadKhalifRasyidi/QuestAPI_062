7005563632f6417c773d22ce5a278ead8b3511f5
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2024-12-30 14:25:09 +0700

    Menambahkan HomeViewModel untuk mengelola state UI mahasiswa dan operasi pengambilan serta penghapusan data

--- app/src/main/java/com/example/meet13/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/meet13/ui/viewmodel/HomeViewModel.kt
@@ -1,6 +1,15 @@
 package com.example.meet13.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.meet13.model.Mahasiswa
+import com.example.meet13.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
 
 sealed class HomeUiState {
     data class Success(val mahasiswa: List<Mahasiswa>) : HomeUiState()
@@ -8,3 +17,36 @@ sealed class HomeUiState {
     object Loading : HomeUiState()
 }
 
+class HomeViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
+    var mhsUIState: HomeUiState by mutableStateOf(HomeUiState.Loading)
+        private set
+
+    init {
+        getMhs()
+    }
+
+    fun getMhs() {
+        viewModelScope.launch {
+            mhsUIState = HomeUiState.Loading
+            mhsUIState = try {
+                HomeUiState.Success(mhs.getMahasiswa())
+            } catch (e: IOException) {
+                HomeUiState.Error
+            } catch (e: HttpException) {
+                HomeUiState.Error
+            }
+        }
+    }
+
+    fun deleteMhs(nim: String) {
+        viewModelScope.launch {
+            try {
+                mhs.deleteMahasiswa(nim)
+            } catch (e: IOException) {
+                HomeUiState.Error
+            } catch (e: HttpException) {
+                HomeUiState.Error
+            }
+        }
+    }
+}
