bc5621daf1cc047e093a7e85a6f07718af26b274
MuhammadKhalifRasyidi <khalifrasyidi@gmail.com>
2024-12-30 14:06:04 +0700

    Menambahkan kelas MahasiswaContainer yang mengimplementasikan AppContainer, menyediakan inisialisasi Retrofit dan MahasiswaRepository untuk akses ke API mahasiswa

--- app/src/main/java/com/example/meet13/dependencies/ContainerApp.kt
+++ app/src/main/java/com/example/meet13/dependencies/ContainerApp.kt
@@ -1,8 +1,30 @@
 package com.example.meet13.dependencies
 
 import com.example.meet13.repository.MahasiswaRepository
+import com.example.meet13.repository.NetworkMahasiswaRepository
+import com.example.meet13.service.MahasiswaService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
 
 interface AppContainer {
     val kontakRepository: MahasiswaRepository
 }
 
+class MahasiswaContainer : AppContainer {
+    private val baseUrl = "http://10.0.2.2.8080/umyTI/" //localhost diganti ip kalo run di hp
+    private val json = Json { ignoreUnknownKeys = true }
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl).build()
+
+
+    private val mahasiswaService: MahasiswaService by lazy{
+        retrofit.create(MahasiswaService::class.java) }
+
+    override val kontakRepository: MahasiswaRepository by lazy {
+        NetworkMahasiswaRepository(mahasiswaService)
+    }
+}
